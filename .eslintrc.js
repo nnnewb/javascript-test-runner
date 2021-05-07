ced2b859c2d5cd48f31f38797dedcc0b16043fb8
liuweiqing <weak_ptr@outlook.com>
2021-05-07 17:07:06 +0800

    build(npm): use eslint instead of tslint

---
+++ .eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+    root: true,
+    parser: "@typescript-eslint/parser",
+    preset: "env",
+    plugins: ["@typescript-eslint"],
+    extends: ["eslint:recommended", "plugin:@typescript-eslint/recommended"],
+};
